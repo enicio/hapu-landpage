ed70c4845bde79f7eef9e159f4ffc247e773b1cf
enicio <jessandro42@gmail.com>
2021-09-08 00:59:56 -0300

    Implemented the import of images from public folder

--- src/components/pageContent/pageContent.jsx
+++ src/components/pageContent/pageContent.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import './pageContent.scss'
 
+//https://create-react-app.dev/docs/using-the-public-folder
 function PageContent({ props }) {
 	return (
 		<section className="content">
+			{props.image
+				? <img src={`/assets/images/${props.image}`} alt={props.alt} />
+				: ''
+			}
 			<h2>
 				{props.title}
 			</h2>
@@ -14,6 +19,7 @@ function PageContent({ props }) {
 				?<a href="#section">{props.link}</a>
 				: ''
 			}
+			<hr/>
 		</section>
 	)
 }
--- src/utils/info.js
+++ src/utils/info.js
@@ -6,31 +6,39 @@ const contents = {
 							with Hapu. Hapu means tribe and it’s our foundational 3 tribal principles that empowers you to create
 							and manage your own tribe. A tribe that together has the power to create new affordable solutions in
 							childcare that work for you and your community.`,
-		link: 'Ready to get started?'
+		link: 'Ready to get started?',
+		image: 'ImageSection1.png',
+		alt: 'Example showing the price and location of nanny in the aplication screen'
 	},
 	section2: {
 		title: 'Are you a parent without a nanny and looking to share?',
-		content: `Leave us your name and email and we’ll update you as soon as a share becomes available in your area!`
+		content: `Leave us your name and email and we’ll update you as soon as a share becomes available in your area!`,
+		image: false
 	},
 	section3: {
 		title: 'Shared payments made simple',
 		content: `Sometimes it’s hard enough just sharing a restaurant bill. Try sharing that bill week in, week out and
 							you might encounter more than a few snares. But not with Hapu. Simply set your rates and our automated
 							payment system takes care of the rest. You need never talk money or who owes what.`,
-		link: 'Read how Bridget’s share (without Hapu) ended over $15'
+		link: 'Read how Bridget’s share (without Hapu) ended over $15',
+		image: 'ImageSection3.png',
+		alt: "Example of price in the application"
 	},
 	section4: {
 		title: 'A framework built for the long term',
 		content: `Childcare is for the long term. And you need a framework you can count on that gives your share long
 							term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly
 							defined process, transparency over money and equality of participation.`,
-		link: 'Read how Hapu’s tribal background defines our app '
+		link: 'Read how Hapu’s tribal background defines our app ',
+		image: false
 	},
 	section5: {
 		title: 'Coming soon: Nanny Share Daily Diary!',
 		content: `With the Hapu daily diary your nanny will be able to update you and your sharers with photos and
 							commentary of the day. You and sharers will receive notifications and you’ll be able to login to
-							view the daily adventures fo your little ones. We can’t wait!`
+							view the daily adventures fo your little ones. We can’t wait!`,
+		image: 'ImageSection5.png',
+		alt: 'Example of daily diary',
 	}
 }
 
